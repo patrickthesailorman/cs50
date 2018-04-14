3f48d32a5a2209d722429b3e3857ed82e5fda44b
Patrick McCarty <patrickthesailorman@gmail.com>
2018-04-14 15:04:58 +0000

    WIP caesar

--- pset2/caesar.c
+++ pset2/caesar.c
@@ -1,18 +1,24 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main(int argc, string argv[])
 {
-    if (argc == 0 || argc > 1)
+    if (argc == 0 || argc < 2)
     {
-        printf("error")
+        printf("error");
         return 1;
     }
     else
     {
-        printf()
+       int k = atoi(argv[1]);
+       printf("enter passphrase: \n");
+       string pp = get_string();
+       for ( char i = 0; i < strlen(argv[1]); i++ )
+        string ec = i - k;
+        printf("%s",pp);
     }
 
-        int k = argv;
+
 }
