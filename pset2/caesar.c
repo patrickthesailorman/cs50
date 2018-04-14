757eb99baca8d42e5d5e2fdf362764a04bd21bec
Patrick McCarty <patrickthesailorman@gmail.com>
2018-04-14 23:26:11 +0000

    WIP caesar

--- pset2/caesar.c
+++ pset2/caesar.c
@@ -1,10 +1,12 @@
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 int main(int argc, string argv[])
 {
+    char ct;
     if (argc == 0 || argc < 2)
     {
         printf("error");
@@ -13,15 +15,28 @@ int main(int argc, string argv[])
     else
     {
        int k = atoi(argv[1]);
-       printf("enter passphrase: \n");
-       string pp = get_string();
-       for ( char i = 0; i < strlen(pp); i++ )
-       if (i == 'A'-'Z' || i == 'a'-'z')
+       printf("plaintext: ");
+       string pt = get_string();
+       printf("ciphertext: ");
+       for ( int i = 0, n = strlen(pt); i < n; i++ )
+       if (isalpha(pt[i]))
        {
-        int ec = (pp[i] + k) % 26;
-        printf("%c",ec);
+           if ( isupper(pt[i]))
+           {
+            ct = (pt[i] -'A' + k) % 26 + 'A';
+           }
+           else if ( islower(pt[i]))
+           {
+            ct = (pt[i] - 'a' + k) % 26 + 'a';
+           }
+        printf("%c", ct);
        }
+       else
+       {
+           ct = pt[i];
+       }
+       printf(" ");
     }
-
+        printf("\n");
 
 }
