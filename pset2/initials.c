87844e66e5b39acad8756fa59304e4a39bded22f
Patrick McCarty <patrickthesailorman@gmail.com>
2018-04-14 01:58:35 +0000

    initials checks out

--- pset2/initials.c
+++ pset2/initials.c
@@ -4,29 +4,29 @@
 #include <string.h>
 
 
-int main (void)
+int main(void)
 {
     // int n;
     string name;
     do
     {
-     name = get_string("Name: "); // Get User input
+        printf("Enter your name:\n");
+        name = get_string(); // Get User input
     }
     while (name == NULL);
 
-     // Iterate over chars of string
-     for (int i = 0, n = strlen(name); i < n; i ++)
-     {
-        if (name[i] == name[0])
+    // Iterate over chars of string
+    for (int i = 0, n = strlen(name); i < n; i ++)
+    {
+        if (name[i] == name[0]) // Capitalize first initial
         {
             printf("%c", toupper(name[i]));
         }
-        else
-        if (name[i] == ' ')
+        else if (name[i] == ' ') // get next character after space and capitalize
         {
             i ++;
             printf("%c", toupper(name[i]));
         }
-     }
-     printf("\n");
+    }
+    printf("\n");
 }
