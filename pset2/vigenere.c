80b0502b994d17dd4ac0f831a465bd556d637150
Patrick McCarty <patrickthesailorman@gmail.com>
2018-04-15 14:59:01 +0000

    WIP vigenere

--- pset2/vigenere.c
+++ pset2/vigenere.c
@@ -19,35 +19,49 @@ int main(int argc, string argv[])
         printf("plaintext: ");
         string pt = get_string(); // prompts user for string to encrypt
         printf("ciphertext: ");
-        char k = strlen(argv[1]); // set key from string to integer
-        if (isalpha(k))
-        {
-            for (int j = 0, m = strlen(k); j < m; j++) // Iterate over key
-            {
-
-                for (int i = 0, n = strlen(pt); i < n; i++) // iterate through the string
-                    if (isalpha(pt[i])) // check if character is alphabetic
+        int k = strlen(argv[1]); // set key from string to integer
+        for (int j = 0, m = strlen(k); j < m; j++) // Iterate over key
+            if (isalpha(k[j])) // check if character is alphabetic
+                {
+                    if (isupper(k[j])) // preserve case of character
                     {
-                        if (isupper(pt[i])) // preserve case of character
-                        {
-                            ct = (pt[i] - 'A' + k[j]) % 26 + 'A'; // to ascii to alphabetic index
-                            printf("%c", ct);
-                        }
-                        else if (islower(pt[i]))
-                        {
-                            ct = (pt[i] - 'a' + k[j]) % 26 + 'a';
-                            printf("%c", ct);
-                        }
+                        k[j] = k[j] - (int)'A'; // to ascii to alphabetic index
 
                     }
-                    else if (!isalpha(pt[i])) // includes special characters
+                    else if (islower(pt[i]))
                     {
-                        ct = pt[i];
+                        k[j] = k[j] - (int)'a';
+                    }
+
+                }
+                else  // includes special characters
+                {
+                    k[j] = k[j];
+                }
+
+            for (int i = 0, n = strlen(pt); i < n; i++) // iterate through the string
+                if (isalpha(pt[i])) // check if character is alphabetic
+                {
+                    if (isupper(pt[i])) // preserve case of character
+                    {
+                        ct = (pt[i] - 'A' + k[j]) % 26 + 'A'; // to ascii to alphabetic index
+                        printf("%c", ct);
+                    }
+                    else if (islower(pt[i]))
+                    {
+                        ct = (pt[i] - 'a' + k[j]) % 26 + 'a';
                         printf("%c", ct);
                     }
 
-            }
-            printf("\n");
+                }
+                else if (!isalpha(pt[i])) // includes special characters
+                {
+                    ct = pt[i];
+                    printf("%c", ct);
+                }
+
         }
+        printf("\n");
     }
+
 }
