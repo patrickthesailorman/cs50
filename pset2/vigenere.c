3bfc485bcd2a76fde27df499e0c96226db094c8b
Patrick McCarty <patrickthesailorman@gmail.com>
2018-04-15 18:30:13 +0000

    WIP vigenere

--- pset2/vigenere.c
+++ pset2/vigenere.c
@@ -16,51 +16,52 @@ int main(int argc, string argv[])
     }
     else
     {
-    int k = strlen(argv[1]); // set key from string to integer
+    string k = argv[1]; // Set key
+    string pt = get_string("plaintext: "); // Prompts user for string to encrypt
+    printf("ciphertext: "); // Print ciphertext here
+    int klen = strlen(argv[1]); // Set key length to integer
+
         for (int j = 0, m = k; j < m; j++) // Iterate over key
+        {
             if (isalpha(j)) // check if character is alphabetic
                 {
                     if (isupper(j)) // preserve case of character
                     {
                         j = j - (int)'A'; // to ascii to alphabetic index
-
                     }
                     else if (islower(j))
                     {
                         j = j - (int)'a';
                     }
-
                 }
                 else  // includes special characters
                 {
                     j++;
                 }
-        printf("plaintext: ");
-        string pt = get_string(); // prompts user for string to encrypt
-        printf("ciphertext: ");
-            for (int i = 0, n = strlen(pt); i < n; i++) // iterate through the string
-                if (isalpha(pt[i])) // check if character is alphabetic
-                {
-                    if (isupper(pt[i])) // preserve case of character
+
+
+                for (int i = 0, n = strlen(pt); i < n; i++) // iterate through the string
+                    if (isalpha(pt[i])) // check if character is alphabetic
                     {
+                        if (isupper(pt[i])) // preserve case of character
+                        {
                         ct = (pt[i] - 'A' + k[j]) % 26 + 'A'; // to ascii to alphabetic index
                         printf("%c", ct);
-                    }
-                    else if (islower(pt[i]))
-                    {
+                        }
+                        else if (islower(pt[i]))
+                        {
                         ct = (pt[i] - 'a' + k[j]) % 26 + 'a';
                         printf("%c", ct);
-                    }
+                        }
 
-                }
-                else if (!isalpha(pt[i])) // includes special characters
-                {
+                    }
+                    else if (!isalpha(pt[i])) // includes special characters
+                    {
                     ct = pt[i];
                     printf("%c", ct);
-                }
+                    }
 
         }
         printf("\n");
-    }
 
 }
