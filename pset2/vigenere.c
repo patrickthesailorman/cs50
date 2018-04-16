93576b61a6e9c5a5deaf25db92fee8735fa3fe6c
Patrick McCarty <patrickthesailorman@gmail.com>
2018-04-16 03:41:15 +0000

    WIP

--- pset2/vigenere.c
+++ pset2/vigenere.c
@@ -6,6 +6,10 @@
 
 // vigenere encryption
 
+int key_check();
+int klen;
+
+
 int main(int argc, string argv[])
 {
     if (argc == 0 || argc < 2 || argc > 2) // requires at least one argument to be the alphabetical key to cipher
@@ -13,12 +17,17 @@ int main(int argc, string argv[])
         printf("error");
         return 1;
     }
+    // else if (!strcheck(k) )
+    // {
+    //   printf("Usage : ./vigenere k\n");
+    //   return 1;
+    // }
     else
     {
         string k = argv[1]; // Set key
+        klen = strlen(argv[1]); // Set key length to integer
         string pt = get_string("plaintext: "); // Prompts user for string to encrypt
         printf("ciphertext: "); // Print ciphertext here
-        int klen = strlen(argv[1]); // Set key length to integer
 
         for (int i = 0, j = strlen(pt), kw = 0; i < j; i++) // Iterate though (i = count. j = strlength kw = keyword)
         {
@@ -42,4 +51,22 @@ int main(int argc, string argv[])
         }
         printf("\n");
     }
+}
+
+
+int key_check(k) // function to verify key is all ABC chars
+{
+    for (int i = 0, j = klen; i < j; i++)
+    {
+        if (isalpha(k[i]))
+        {
+            i++;
+        }
+        else // if the char is not abc, return error
+        {
+            return 1;
+            break;
+        }
+    }
+    return 0;
 }
