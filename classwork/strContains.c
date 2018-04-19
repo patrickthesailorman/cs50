15c9012c9ed3a83b6165da8078f017246b2c49aa
Patrick McCarty <patrickthesailorman@gmail.com>
2018-04-19 21:15:34 +0000

    WIP strContains

--- classwork/strContains.c
+++ classwork/strContains.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 
-string strContains();
+string strContains(string str1, string subStr);
 
 int main(int argc, string argv[])
 {
@@ -14,16 +14,16 @@ int main(int argc, string argv[])
       }
       else if (argc == 3 )
       {
-        string str1 = get_string("Enter a string:\n");
-        string subStr = get_string("Enter string to find:\n");
-        strContains(argv[1], argv[2]);
+        string str1 = argv[1];
+        string subStr = argv[2];
+       // string str1 = get_string("Enter a string:\n");
+       // string subStr = get_string("Enter string to find:\n");
+        strContains(str1, subStr);
       }
 }
-    string strContains(string str1, substr);
+    string strContains(string str1, string subStr);
     {
-        str1 = str1;
-        subStr = subStr;
-        char * strstr(const char * str1, const char * substr);
+        char * strstr(const char * str1, const char * subStr);
         if (true)
         {
             printf("The string exists!\n");
@@ -36,7 +36,7 @@ int main(int argc, string argv[])
         }
 
       }
-    }
+
 
 
 
