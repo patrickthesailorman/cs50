33c90728b3bd2d938667cf73cd48d1a02db4b40e
Patrick McCarty <patrickthesailorman@gmail.com>
2018-04-23 23:38:18 +0000

    Vigenere works now!

--- pset2/vigenere.c
+++ pset2/vigenere.c
@@ -6,24 +6,6 @@
 
 // vigenere encryption
 
-// int kw_check(string input)
-//     {
-//         int i = 0;
-//         while (input[i])
-//         {
-//         if (!isalpha(input[i]))
-//         {
-//             // return false if non-alpha character
-
-//             return 0;
-//         }
-//         toupper([i]);
-//         i++;
-//         }
-//         // return true if only letters
-//         return 1;
-//     }
-
 int main(int argc, string argv[])
 {
     if (argc != 2)
@@ -46,31 +28,31 @@ int main(int argc, string argv[])
 
     }
 
-        string k = argv[1]; // Set key
-        int klen = strlen(k); // Set key length to integer
-        string pt = get_string("plaintext: "); // Prompts user for string to encrypt
-        printf("ciphertext: "); // Print ciphertext here
+    string k = argv[1]; // Set key
+    int klen = strlen(k); // Set key length to integer
+    string pt = get_string("plaintext: "); // Prompts user for string to encrypt
+    printf("ciphertext: "); // Print ciphertext here
 
-        for (int i = 0, j = strlen(pt), kw = 0; i < j; i++) // Iterate though (i = count. j = strlength kw = keyword)
+    for (int i = 0, j = strlen(pt), kw = 0; i < j; i++) // Iterate though (i = count. j = strlength kw = keyword)
+    {
+        int lower_key = tolower(k[kw % klen]) - 'a'; // set key to lowercase
+
+        if (isupper(pt[i])) // preserve case of character
         {
-            int lower_key = tolower(k[kw % klen]) - 'a'; // set key to lowercase
-
-                if (isupper(pt[i])) // preserve case of character
-                {
-                    printf("%c", 'A' + (pt[i] - 'A' + lower_key) % 26); // ABC index, shift by key but wraparound, return to ASCII
-                    kw++;
-                }
-                else if (islower(pt[i]))
-                {
-                    printf("%c", 'a' + (pt[i] - 'a' + lower_key) % 26); // ABC index, shift by key but wraparound, return to ASCII
-                    kw++;
-                }
-
-            else  // includes special characters
-            {
-                printf("%c", pt[i]);
-            }
+            printf("%c", 'A' + (pt[i] - 'A' + lower_key) % 26); // ABC index, shift by key but wraparound, return to ASCII
+            kw++;
         }
-        printf("\n");
+        else if (islower(pt[i]))
+        {
+            printf("%c", 'a' + (pt[i] - 'a' + lower_key) % 26); // ABC index, shift by key but wraparound, return to ASCII
+            kw++;
+        }
+
+        else  // includes special characters
+        {
+            printf("%c", pt[i]);
+        }
+    }
+    printf("\n");
 
 }
