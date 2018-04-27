54210c3adbeddb43d5e53698dbf5abf4e1dc55af
Patrick McCarty <patrickthesailorman@gmail.com>
2018-04-27 02:20:52 +0000

    Add test

--- pset3/music/test.c
+++ pset3/music/test.c
@@ -1,20 +1,36 @@
 
 #include <cs50.h>
+#include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 
 int main(void)
 {
-    string s = get_string("string s");
+   string fraction = get_string("fraction\n");
+
+    for (int i = 0, j = strlen(fraction); i < j; i++)
     {
-        int rest_compare = strcmp(s, "");
-        if (rest_compare == 0)
-        {
-        return true;
-        }
-        else
-        {
-        return false;
-        }
+    int numerator = fraction[0];
+    int denominator = fraction[2];
+
+    switch(denominator)
+    {
+      case 56 : // '1/8'
+         return numerator - '0';
+         break;
+      case 52 : // '1/4'
+         printf("%i\n", (numerator - '0') * 2);
+         return (numerator - '0') * 2;
+         break;
+      case 50 : // '1/2'
+        printf("%i\n", (numerator - '0') * 4);
+         return (numerator - '0') * 4;
+         break;
+
+      default :
+         return 0;
     }
+    }
+
 }
