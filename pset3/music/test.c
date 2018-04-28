9aa402b435e0e8a35397f9fdc9a85df24448ea91
Patrick McCarty <patrickthesailorman@gmail.com>
2018-04-28 17:23:06 +0000

    Add test frequency

--- pset3/music/test.c
+++ pset3/music/test.c
@@ -13,88 +13,93 @@ int main(void)
         {
             char key = note[0];
             char accidental = note[1];
-            int octave =note[2];
+            int octave = note[2];
 
-        switch(note[0])
-        {
-            case 'C':
-            if (note[1] == '#' )
+            switch (note[0])
             {
-              return 440 / pow(2.0, (8.0 / 12.0));
-            }
-            else if (note[1] == 'b')
-            {
-              return 440 / pow(2.0, (10.0 / 12.0));
-            }
-            return 440 / pow(2.0, (9.0 / 12.0));
-            break;
+                case 'C':
+                    if (note[1] == '#')
+                    {
+                        return 440 / pow(2.0, (8.0 / 12.0));
+                    }
+                    else if (note[1] == 'b')
+                    {
+                        return 440 / pow(2.0, (10.0 / 12.0));
+                    }
+                    return 440 / pow(2.0, (9.0 / 12.0));
+                    break;
 
-            case 'D':
-            if (note[1] == '#' )
-            {
-              return 440 / pow(2.0, (6.0 / 12.0));
-            }
-            else if (note[1] == 'b')
-            {
-              return 440 / pow(2.0, (8.0 / 12.0));
-            }
-            return 440 / pow(2.0, (7.0 / 12.0));
-            break;
+                case 'D':
+                    if (note[1] == '#')
+                    {
+                        return 440 / pow(2.0, (6.0 / 12.0));
+                    }
+                    else if (note[1] == 'b')
+                    {
+                        return 440 / pow(2.0, (8.0 / 12.0));
+                    }
+                    return 440 / pow(2.0, (7.0 / 12.0));
+                    break;
 
-            case 'E':
-            if (note[1] == '#' )
-            {
-              return 440 / pow(2.0, (4.0 / 12.0));
-            }
-            else if (note[1] == 'b')
-            {
-              return 440 / pow(2.0, (6.0 / 12.0));
-            }
-            return 440 / pow(2.0, (5.0 / 12.0));
-            break;
+                case 'E':
+                    if (note[1] == '#')
+                    {
+                        return 440 / pow(2.0, (4.0 / 12.0));
+                    }
+                    else if (note[1] == 'b')
+                    {
+                        return 440 / pow(2.0, (6.0 / 12.0));
+                    }
+                    return 440 / pow(2.0, (5.0 / 12.0));
+                    break;
 
-            case 'F':
-            if (note[1] == '#' )
-            {
-              return 440 / pow(2.0, (3.0 / 12.0));
-            }
-            else if (note[1] == 'b')
-            {
-              return 440 / pow(2.0, (5.0 / 12.0));
-            }
-            return 440 / pow(2.0, (4.0 / 12.0));
-            break;
+                case 'F':
+                    if (note[1] == '#')
+                    {
+                        return 440 / pow(2.0, (3.0 / 12.0));
+                    }
+                    else if (note[1] == 'b')
+                    {
+                        return 440 / pow(2.0, (5.0 / 12.0));
+                    }
+                    return 440 / pow(2.0, (4.0 / 12.0));
+                    break;
 
-            case 'G':
-            return 440 / pow(2.0, (2.0 / 12.0));
-            break;
+                case 'G':
+                    if (note[1] == '#')
+                    {
+                        return 440 / pow(2.0, (3.0 / 12.0));
+                    }
+                    else if (note[1] == 'b')
+                    {
+                        return 440 / pow(2.0, (4.0 / 12.0));
+                    }
+                    return 440 / pow(2.0, (2.0 / 12.0));
+                    break;
 
-            case 'A':
-            if (note[1] == '#' )
-            {
-              return 440 * pow(2.0, (1.0 / 12.0));
+                case 'A':
+                    if (note[1] == '#')
+                    {
+                        return 440 * pow(2.0, (1.0 / 12.0));
+                    }
+                    else if (note[1] == 'b')
+                    {
+                        return 440 / pow(2.0, (1.0 / 12.0));
+                    }
+                    return 440;
+                    break;
+                case 'B':
+                    if (note[1] == '#')
+                    {
+                        return 440 * pow(2.0, (3.0 / 12.0));
+                    }
+                    else if (note[1] == 'b')
+                    {
+                        return 440 * pow(2.0, (1.0 / 12.0));
+                    }
+                    return 440 * pow(2.0, (2.0 / 12.0));
+                    break;
             }
-            else if (note[1] == 'b')
-            {
-              return 440 / pow(2.0, (1.0 / 12.0));
-            }
-            else
-            {
-            return 440;
-            }
-              break;
-            case 'B':
-            if (note[1] == '#' )
-            {
-              return 440 * pow(2.0, (3.0 / 12.0));
-            }
-            else if (note[1] == 'b')
-            {
-              return 440 * pow(2.0, (1.0 / 12.0));
-            }
-              return 440 * pow(2.0, (2.0 / 12.0));
-              break;
-        }
         }
         else
         {
