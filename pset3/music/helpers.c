e36bf853a4645dc12ec32ef13be200a222b78162
Patrick McCarty <patrickthesailorman@gmail.com>
2018-04-29 01:12:08 +0000

    Passes check50

--- pset3/music/helpers.c
+++ pset3/music/helpers.c
@@ -1,6 +1,7 @@
 // Helper functions for music
 
 #include <cs50.h>
+#include <math.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -11,56 +12,120 @@ int duration(string fraction)
 {
     for (int i = 0, j = strlen(fraction); i < j; i++)
     {
-    int numerator = fraction[0];
-    int denominator = fraction[2];
+        int numerator = fraction[0]; // Get the numerator
+        int denominator = fraction[2]; // Get the denominator
+        int eighths; // Return value
 
-    switch(denominator)
-    {
-      case 56 : // '1/8'
-         return numerator - '0';
-         break;
-      case 52 : // '1/4'
-         return (numerator - '0') * 2;
-         break;
-      case 50 : // '1/2'
-         return (numerator - '0') * 4;
-         break;
+        switch (denominator) // Math to return the result in eighths
+        {
+            case 56 : // '1/8'
+                eighths = numerator - '0';
+                break;
+            case 52 : // '1/4'
+                eighths = (numerator - '0') * 2;
+                break;
+            case 50 : // '1/2'
+                eighths = (numerator - '0') * 4;
+                break;
 
-      default :
-         return 0;
+            default :
+                return 0;
+        }
+        return eighths;
     }
-    }
+    return 20;
 }
 
 // Calculates frequency (in Hz) of a note
 int frequency(string note)
 {
-    if (strlen(note) == 3)
-    {
-        int key = note[0];
-        char accidental = note[1];
-        int octave =note[2];
-    }
-    else
-    {
+    int octave;
 
-        key = note[0];
-        octave = note[1];
-    }
-    switch(note[0])
     {
-        case 'A':
-        if (note[1] == '#' && note[2] == 4)
+        if (strlen(note) == 3) // if string contains accidental('#' or 'b') adjust character order for octave.
         {
-          return 440;
+            octave = note[2];
         }
-          break;
-        case 'A5':
-          return math.round(440 * 2);
-          break;
+        else
+        {
+            octave = note[1];
+        }
+
+        double frequency; // set frequency to double data type
+
+        switch (note[0]) // Find the note for first character
+        {
+            case 67: // C
+                frequency = 440.0 / pow(2.0, (9.0 / 12.0));
+                break;
+
+            case 68: // D
+                frequency = 440.0 / pow(2.0, (7.0 / 12.0));
+                break;
+
+            case 69: // E
+                frequency = 440.0 / pow(2.0, (5.0 / 12.0));
+                break;
+
+            case 70: // F
+                frequency = 440.0 / pow(2.0, (4.0 / 12.0));
+                break;
+
+            case 71: // G
+                frequency = 440.0 / pow(2.0, (2.0 / 12.0));
+                break;
+
+            case 65: // A
+                frequency = 440.0;
+                break;
+            case 66: // B
+                frequency = 440.0 * pow(2.0, (2.0 / 12.0));
+                break;
+        }
+
+        if (note[1] == '#') // shift frequency for sharp or flat
+        {
+            frequency *=  pow(2.0, (1.0 / 12.0));
+        }
+        else if (note[1] == 'b')
+        {
+            frequency /= pow(2.0, (1.0 / 12.0));
+        }
+
+
+        switch (octave) // Shift frequncy for octave
+        {
+            case 53: // octave 5
+                frequency = frequency * 2;
+                break;
+            case 54: // octave 6
+                frequency = frequency * 4;
+                break;
+            case 55: // octave 7
+                frequency = frequency * 8;
+                break;
+            case 56: // octave 8
+                frequency = frequency * 16;
+                break;
+            case 51: // octave 3
+                frequency = frequency / 2;
+                break;
+            case 50: // octave 2
+                frequency = frequency / 4;
+                break;
+            case 49: // octave 1
+                frequency = frequency / 8;
+                break;
+            case 48: // octave 0
+                frequency = frequency / 16;
+                break;
+        }
+        int freqHz = round(frequency); // round it off
+
+        return freqHz;
+
     }
 }
-
 // Determines whether a string represents a rest
 bool is_rest(string s)
 {
@@ -68,10 +133,10 @@ bool is_rest(string s)
 
     if (rest_compare == 0)
     {
-    return true;
+        return true;
     }
     else
     {
-    return false;
+        return false;
     }
 }
