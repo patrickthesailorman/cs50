a5fdf74a1a52658f7dc777e74cc30cf8e08a61a1
Patrick McCarty <patrickthesailorman@gmail.com>
2018-05-15 01:15:02 +0000

    Add pseudo code

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -11,4 +11,22 @@ int main(int argc, char *argv[])
         fprintf(stderr, "Usage: ./recover image\n");
         return 1;
     }
+
+
+
+
+    // Recall the possibility of slack space. odds are it’d been "zeroed" (i.e., filled with 0s)
+    // find that the image contains 50 JPEGs.
+
+    // open card file
+    // repeat until end of card
+    //  read 512 bytes into a buffer
+    //  start of a new JPEG?
+    //      Yes =>
+    //      No =>
+    // Already found a JPEG?
+    //      Yes =>
+    //      No =>
+    // Close any remaining files
+
 }
