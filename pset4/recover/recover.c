8819859d3a6fa77176054912d91b2c728ad7bc9b
Patrick McCarty <patrickthesailorman@gmail.com>
2018-05-17 03:29:38 +0000

    WIP

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -1,7 +1,9 @@
 // File to recover .jpeg data
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-#include "card.raw"
+// #include "card.raw"
 
 int main(int argc, char *argv[])
 {
@@ -11,7 +13,10 @@ int main(int argc, char *argv[])
         fprintf(stderr, "Usage: ./recover image\n");
         return 1;
     }
+    typedef uint8_t BYTE;
     FILE *file = fopen(argv[1], "r");
+    FILE *img = NULL;
+    // char fileName[8]; //JPEG filename
 
     if(file == NULL) // Check for NULL
     {
@@ -19,21 +24,31 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    char buffer[512];
-    char filename[50];
+    BYTE buffer[512];
+    char filename[50]; //JPEG filename
     int x = 1; // JPEG filecounter
 
-    while(fread(buffer, 512, 1, argv[1]) == 1) // Iterate over a copy
+    // While not EOF
+    while (feof(file) == 0) {
+    fread(buffer, 512, 1, argv[1]) == 1) // Iterate over a copy
     {
         // Find the first three bytes of JPEGs: 0xff 0xd8 0xff
         if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0) // last byte: 0xe0, 0xe1, 0xe2.....0xef
         {
-                if()
+            if (x != 0) {
+                fclose(*img);
+            }
+             // Open a new jpg file
+
             sprintf(filename, "%03i.jpg", x);
             // filename: char array to store resultant string
-            FILE *img = fopen(filename, "w");
+            *img = fopen(filename, "w");
             x++; // Increment file count
-            fwrite(buffer, 512, 1, *img); // Write to file units of 512 B
+
+            if (x != 0) {
+                fwrite(buffer, 512, 1, *img); // Write to file units of 512 B
+            }
+
         }
         else if(x = 1 && buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
@@ -52,6 +67,7 @@ int main(int argc, char *argv[])
     }
     // close current file and open new one IF THIS IS A JPEG && YOUVE FOUND A NEW ONE
 fclose(*img);
+fclose(*file);
 
 
     // Recall the possibility of slack space. odds are it’d been "zeroed" (i.e., filled with 0s)
