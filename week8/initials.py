2d0a8dfe5b171f41c8d288a6bab275132d97ca64
Patrick McCarty <patrickthesailorman@gmail.com>
2018-08-22 00:09:12 +0000

    Add c.isupper()

--- week8/initials.py
+++ week8/initials.py
@@ -3,6 +3,6 @@ from cs50 import get_string
 s = get_string("name: ")
 initials = ""
 for c in s:
-    if c is an uppercase letter:
+    if c.isupper():
         append c to initials
 print(initials)
