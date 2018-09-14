0b82e026c2da88bb0511161c2e6b5e4b5626475e
Patrick McCarty <patrickthesailorman@gmail.com>
2018-09-14 23:28:08 +0000

    Add mario

--- pset6/mario.py
+++ pset6/mario.py
@@ -1,3 +1,40 @@
 from cs50 import get_int
 
 n =  get_int("Number: ")
+
+# Print out this many rows
+for i in range(n):
+
+    # Print out this many columns
+    for j in range(n - i - 1):
+         print(" ", end="")
+    print()
+
+
+# int main(void)
+# {
+#     int height;
+#     do
+#     {
+#         printf("How many steps will Mario climb?\n");
+#         height = get_int(); // User inputs steps
+#     }
+#     while (height < 0 || height > 23); // Validate steps
+
+#     for (int i = 0; i < height; i++) // Loop line counter
+#     {
+
+
+#         for (int x = 0; x < height - i - 1; x++) //Loop decreasing spaces
+#         {
+#             printf(" ");
+#         }
+
+#         for (int y = 0; y < 2 + i; y++) // Loop increasing #'s'
+#         {
+#             printf("#");
+#         }
+
+#         printf("\n");
+#     }
+# }
