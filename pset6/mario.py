87c094f88c9b2ff1f58d8679caabeb7765017bb4
Patrick McCarty <patrickthesailorman@gmail.com>
2018-09-15 15:51:19 +0000

    working

--- pset6/mario.py
+++ pset6/mario.py
@@ -6,43 +6,10 @@ while True:
         break
 
 # Print out this many rows
-i = 0
 for i in range(n):
-
     # Print out this many columns
-    j = 0
-    for j in range(n - i - 1):
+    for j in range(n - i):
         print(" ", end="")
-        k = 0
-        for k in range(k < 2 + i):
-            print("#", end="")
+    for k in range(2 + i):
+        print("#", end="")
     print()
-
-
-# int main(void)
-# {
-#     int height;
-#     do
-#     {
-#         printf("How many steps will Mario climb?\n");
-#         height = get_int(); // User inputs steps
-#     }
-#     while (height < 0 || height > 23); // Validate steps
-
-#     for (int i = 0; i < height; i++) // Loop line counter
-#     {
-
-
-#         for (int x = 0; x < height - i - 1; x++) //Loop decreasing spaces
-#         {
-#             printf(" ");
-#         }
-
-#         for (int y = 0; y < 2 + i; y++) // Loop increasing #'s'
-#         {
-#             printf("#");
-#         }
-
-#         printf("\n");
-#     }
-# }
