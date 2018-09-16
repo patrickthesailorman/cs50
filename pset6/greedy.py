36dc118f2a8e441d02fd48900e0aa3f901502c60
Patrick McCarty <patrickthesailorman@gmail.com>
2018-09-16 22:24:24 +0000

    fix increment

--- pset6/greedy.py
+++ pset6/greedy.py
@@ -1,39 +1,35 @@
 from cs50 import get_float
 
-while True:
-    change = get_float("How much change is owed?\n$")
-    if change <= 0.00:
-        break
+# while True:
+change = get_float("How much change is owed?\n$")
+    # if change <= 0.00:
+    #     break
 
 cents = round(change * 100) # Convert float to int
 coins = 0
 
 # Will it take a quater, how many
-while cents >= 25
-
-         coins ++;
-         cents -= 25;
+if cents >= 25:
+    coins +=1
+    cents -= 25
 
 # Will it take a dime, how many
-    while cents >= 10
-
-         coins ++;
-         cents -= 10;
+    while cents >= 10:
+         coins += 1
+         cents -= 10
 
 # Will it take a nickel, how many
-     while cents >= 5
-
-         coins ++;
-         cents -= 5;
+    while cents >= 5:
+        coins += 1
+        cents -= 5
 
 # Remainder in pennies
-     while cents >= 1
-
-         coins ++;
-         cents -= 1;
+    while cents >= 1:
+         coins += 1
+         cents -= 1
 
 # return total number of coins used to make change
-     print(f"Total coins: {coins}")
+    print(f"Total coins: {coins}")
 
 # #include <cs50.h>
 # #include <stdio.h>
