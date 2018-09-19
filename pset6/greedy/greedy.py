43dc5e1b7368b7039698a82a962b9d2552737828
Patrick McCarty <patrickthesailorman@gmail.com>
2018-09-19 23:31:42 +0000

    check50

--- pset6/greedy/greedy.py
+++ pset6/greedy/greedy.py
@@ -1,16 +1,16 @@
 from cs50 import get_float
 
-# while True:
-change = get_float("How much change is owed?\n$")
-    # if change <= 0.00:
-    #     break
+while True:
+    change = get_float("How much change is owed?\n$")
+    if change > 0.00:
+        break
 
 cents = round(change * 100) # Convert float to int
 coins = 0
 
 # Will it take a quater, how many
 while cents >= 25:
-    coins +=1
+    coins += 1
     cents -= 25
 
 # Will it take a dime, how many
@@ -29,4 +29,4 @@ while cents >= 1:
     cents -= 1
 
 # return total number of coins used to make change
-    print(f"Total coins: {coins}")
+print(f"Total coins: {coins}")
