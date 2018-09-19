55ab8d064eaad8d5b08f3bfb93a39c9bea00e3f7
Patrick McCarty <patrickthesailorman@gmail.com>
2018-09-19 00:24:30 +0000

    check50

--- pset6/caesar/caesar.py
+++ pset6/caesar/caesar.py
@@ -8,18 +8,18 @@ import sys
 key = int(sys.argv[1])
 
 plainText = get_string("plaintext: ") # prompts user for string to encrypt
-print("Encrypted: ", end="")
+print("ciphertext: ", end="")
 
 for c in range(len(plainText)): # iterate through each character
     character = plainText[c]
     if character.isalpha(): # check if character is alphabetic
         if character.isupper(): # preserve case of character
-             cipherText = ord(character) - ord('A') + key % 26 + ord('A') # to ascii to alphabetic index
-             cipherText = chr(cipherText)
+             cipherText = (ord(character) - ord('A') + key) % 26 + ord('A') # to ascii
+             cipherText = chr(cipherText) # to alphabetic
              print(cipherText, end="")
         elif character.islower():
-            cipherText = ord(character) - ord('a') + key % 26 + ord('a')
-            cipherText = chr(cipherText)
+            cipherText = (ord(character) - ord('a') + key) % 26 + ord('a') # to ascii
+            cipherText = chr(cipherText) # to alphabetic
             print(cipherText, end="")
     elif not plainText[c].isalpha(): # includes special characters
          cipherText = character;
