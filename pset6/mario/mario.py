b9c05f46163f4ce21bf0cb6b152e6e60480d4d7d
Patrick McCarty <patrickthesailorman@gmail.com>
2018-09-19 01:16:42 +0000

    Check50

--- pset6/mario/mario.py
+++ pset6/mario/mario.py
@@ -2,13 +2,13 @@ from cs50 import get_int
 
 while True:
     n =  get_int("Number: ")
-    if n >= 1:
+    if n >= 0 and n < 24:
         break
 
 # Print out this many rows
 for i in range(n):
     # Print out this many columns
-    for j in range(n - i):
+    for j in range(n - i - 1):
         print(" ", end="")
     for k in range(2 + i):
         print("#", end="")
