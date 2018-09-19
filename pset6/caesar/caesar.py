5360461607e7ca3e208e4367236d45a854fd7d63
Patrick McCarty <patrickthesailorman@gmail.com>
2018-09-19 00:27:20 +0000

    style50

--- pset6/caesar/caesar.py
+++ pset6/caesar/caesar.py
@@ -7,21 +7,21 @@ import sys
 
 key = int(sys.argv[1])
 
-plainText = get_string("plaintext: ") # prompts user for string to encrypt
+plainText = get_string("plaintext: ")  # prompts user for string to encrypt
 print("ciphertext: ", end="")
 
-for c in range(len(plainText)): # iterate through each character
+for c in range(len(plainText)):  # iterate through each character
     character = plainText[c]
-    if character.isalpha(): # check if character is alphabetic
-        if character.isupper(): # preserve case of character
-             cipherText = (ord(character) - ord('A') + key) % 26 + ord('A') # to ascii
-             cipherText = chr(cipherText) # to alphabetic
-             print(cipherText, end="")
-        elif character.islower():
-            cipherText = (ord(character) - ord('a') + key) % 26 + ord('a') # to ascii
-            cipherText = chr(cipherText) # to alphabetic
+    if character.isalpha():  # check if character is alphabetic
+        if character.isupper():  # preserve case of character
+            cipherText = (ord(character) - ord('A') + key) % 26 + ord('A')  # to ascii
+            cipherText = chr(cipherText)  # to alphabetic
             print(cipherText, end="")
-    elif not plainText[c].isalpha(): # includes special characters
-         cipherText = character;
-         print(cipherText, end="");
-print();
+        elif character.islower():
+            cipherText = (ord(character) - ord('a') + key) % 26 + ord('a')  # to ascii
+            cipherText = chr(cipherText)  # to alphabetic
+            print(cipherText, end="")
+    elif not plainText[c].isalpha():  # includes special characters
+        cipherText = character
+        print(cipherText, end="")
+print()
